dd1ebe5fbddef7365ffc5e1caf65caede00b844f
marj <baltazarmarjorie508@gmail.com>
2024-11-16 18:14:27 +0800

    kk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
+
     // Firebase Libraries
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.auth)
@@ -70,4 +71,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
