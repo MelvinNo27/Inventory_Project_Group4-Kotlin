891fbee534c7bf92a0a3c25525516bb480799658
Melvin <159402862+MelvinNo27@users.noreply.github.com>
2024-11-18 10:15:54 +0800

    Melvin Commited in 11/18/2024

--- app/src/main/java/com/example/myapplication/Room14.kt
+++ app/src/main/java/com/example/myapplication/Room14.kt
@@ -1,4 +1,28 @@
 package com.example.myapplication
 
-class Room14 {
-}
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityRoom14Binding
+
+
+class Room14 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRoom14Binding // ViewBinding instance
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        // Initialize ViewBinding
+        binding = ActivityRoom14Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        }
+    }
+
+
+
+
--- app/src/main/java/com/example/myapplication/Room7.kt
+++ app/src/main/java/com/example/myapplication/Room7.kt
@@ -1,4 +1,25 @@
 package com.example.myapplication
 
-class Room7 {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityRoom7Binding
+
+
+class Room7 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRoom7Binding // ViewBinding instance
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        // Initialize ViewBinding
+        binding = ActivityRoom7Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
 }
+
+
